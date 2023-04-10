9075060ae4dfa77d17826e7c781178b23a9eaa3b
LIA715 <sy0715p@naver.com>
2023-04-10 18:38:29 +0900

    23.04.10 webframe

--- blog/urls.py
+++ blog/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path,include #path 함수 사용
 from . import views
 urlpatterns = [
-    path('',views.index), #서버주소 밑에 블로그 server/blog/ => 이게 주소가 됨
+    #path('',views.index), #서버주소 밑에 블로그 server/blog/ => 이게 주소가 됨
+    path('',views.PostList.as_view()), #postlist에서 정해진 모델을 자동으로 가져오는 역할. 함수가 아니고 클래스라 동작을 못해.
+
     path('<int:pk>/',views.single_post_page), #server/blog/숫자 =>가 들어가는 상대적인 경로로 쓰여짐.
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-
+from django.views.generic import ListView,DetailView
 # Create your views here.
 from django.shortcuts import render
 
@@ -8,17 +8,24 @@ from django.shortcuts import render
 from .models import Post #db 접근이 model 접근임. from으로 불러와
 
 # Create your views here.
-def index(request):
-    posts=Post.objects.all() #모두 가져와 all posts=변수.
 
-    return render(
-        request,
-        'blog/index.html',
-        {
-            'posts':posts, #추가적으로 보내고싶은건 더 쓰면됨.
-        }
-    )
+class PostList(ListView): #List에서는 모델명_list.html을 찾도록 자동 설정 되어있음. >>post_list.html이 자동 생성
+    model = Post
+    ordering = '-pk'
+    template_name = 'blog/index.html' #템플릿이 바뀜. 자동으로 바뀜
 
+
+# def index(request):
+#     posts=Post.objects.all() #모두 가져와 all posts=변수.
+#
+#     return render(
+#         request,
+#         'blog/index.html',
+#         {
+#             'posts':posts, #추가적으로 보내고싶은건 더 쓰면됨.
+#         }
+#     )
+#
 def single_post_page(request,pk):
     post = Post.objects.get(pk=pk) #특정 위치의 것을 가져와 get post라는 이름으로 하나만 넘기겠다.
     return render(
