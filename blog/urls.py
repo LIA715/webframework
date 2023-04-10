afb34e308c6d013122bd65aaf89f8b0ec7943029
LIA715 <sy0715p@naver.com>
2023-04-10 17:30:01 +0900

    23.04.10 webframe

--- blog/urls.py
+++ blog/urls.py
@@ -2,5 +2,5 @@ from django.urls import path,include #path 함수 사용
 from . import views
 urlpatterns = [
     path('',views.index),
-    path('<int:pk/>',views.single_post_page),
+    path('<int:pk>',views.single_post_page),
 ]
