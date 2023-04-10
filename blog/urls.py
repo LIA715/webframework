0c26f45ff8459e9accab2dc8612db05cd5ea9df1
LIA715 <sy0715p@naver.com>
2023-04-10 17:26:40 +0900

    23.04.10 webframe

--- blog/urls.py
+++ blog/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path,include #path 함수 사용
-
+from . import views
 urlpatterns = [
-
-
+    path('',views.index),
+    path('<int:pk/>',views.single_post_page),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,26 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.shortcuts import render
+
+# Create your views here.
+from django.shortcuts import render
+from .models import Post
+
+# Create your views here.
+def index(request):
+    posts=Post.objectss.all()
+
+    return render(
+        request,
+        'blog/index.html',
+        {
+            'posts':posts,
+        }
+    )
+
+def single_post_page(request,pk):
+    return render(
+        request,
+        'blog/single_post_page.html',
+    )
