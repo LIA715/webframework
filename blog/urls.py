a801b8461d8b73d4d9dadb5666496fcc3d400ff0
LIA715 <sy0715p@naver.com>
2023-04-10 18:17:13 +0900

    23.04.10 webframe

--- blog/urls.py
+++ blog/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path,include #path 함수 사용
 from . import views
 urlpatterns = [
-    path('',views.index),
-    path('<int:pk>',views.single_post_page),
+    path('',views.index), #서버주소 밑에 블로그 server/blog/ => 이게 주소가 됨
+    path('<int:pk>/',views.single_post_page), #server/blog/숫자 =>가 들어가는 상대적인 경로로 쓰여짐.
 ]
--- blog/views.py
+++ blog/views.py
@@ -5,22 +5,22 @@ from django.shortcuts import render
 
 # Create your views here.
 from django.shortcuts import render
-from .models import Post
+from .models import Post #db 접근이 model 접근임. from으로 불러와
 
 # Create your views here.
 def index(request):
-    posts=Post.objects.all()
+    posts=Post.objects.all() #모두 가져와 all posts=변수.
 
     return render(
         request,
         'blog/index.html',
         {
-            'posts':posts,
+            'posts':posts, #추가적으로 보내고싶은건 더 쓰면됨.
         }
     )
 
 def single_post_page(request,pk):
-    post = Post.objects.get(pk=pk)
+    post = Post.objects.get(pk=pk) #특정 위치의 것을 가져와 get post라는 이름으로 하나만 넘기겠다.
     return render(
         request,
         'blog/single_post_page.html',
